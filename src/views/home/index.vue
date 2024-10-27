301392656a2f7d5548ea958501928f96c9852619
windyeasy <windyeasy13gb@163.com>
2024-10-27 20:10:47 +0800

    feat: improve the layout for the home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -27,17 +27,17 @@ const projects = ref([
 
 <template>
   <div id="home" class="mx-auto max-w-[800px] m-t-[40px]">
-    <div class="title color-[#333] text-xl font-bold">
+    <div class="title color-[#333]  text-xl font-bold">
       Projects
     </div>
-    <div class="projects  grid gap-y-1 gap-x-4 sm:grid-cols-2 lg:grid-cols-3 p-t-5">
+    <div class="projects color-[#666]  grid gap-y-1 gap-x-4 sm:grid-cols-2 lg:grid-cols-3 p-t-5">
       <template v-for="(item) in projects" :key="item.link">
         <router-link :to="item.link" class="mr-4 link">
           {{ item.name }}
         </router-link>
       </template>
     </div>
-    <footer class="footer m-t-5 color-[#333]">
+    <footer class="footer m-t-5 color-[#666]">
       from 2024/10/27
     </footer>
   </div>
