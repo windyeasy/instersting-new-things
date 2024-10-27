fdadef83d8b2f997b69f1644eb3e75b4049419ec
windyeasy <windyeasy13gb@163.com>
2024-10-27 15:23:18 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">instersting-new-things</h1>
+
+Some interesting little projects.
+
+## Run
+
+node > 16.0
+
+### Install
+
+```sh
+pnpm install
+```
+
+### Develop
+
+```sh
+pnpm run dev
+```
+
+### Build
+
+```sh
+pnpm run build
+```
+
+## commint
+
+use: husky + commitlint
+
+- method 1: use cli
+
+```shell
+pnpm run commit
+```
+
+- method 2: use git
+
+```shell
+git commit -m "feat: add a new feature"
+```
+
+## License
+
+instersting-new-things is [MIT](./LICENSE).
---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div id="home">
+    <header class="text-center text-3xl mt20">
+      Vue3+ts template
+    </header>
+    <main class="mt20 flex justify-center items-center flex-col">
+      <img class="logo" src="/vite.svg" alt="logo">
+      <p class="text-lg">
+        The project is designed to fast develop a website.
+      </p>
+    </main>
+    <footer class="text-center mt10">
+      author: <a class="text-gray-700 no-underline hover:underline" href="https://github.com/windyeasy" target="_blank">windyeasy</a>
+    </footer>
+  </div>
+</template>
