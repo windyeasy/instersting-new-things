9d266aaa3d30340781dbbbe57ccd42956b4f486b
windyeasy <windyeasy13gb@163.com>
2024-10-27 16:15:30 +0800

    feat: home page layout

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,44 @@
 <script lang="ts" setup>
+const projects = ref([
+  {
+    name: 'vue3-admin-template',
 
+    link: '/views/projects/001',
+  },
+  {
+    name: 'vue3-admin-template',
+    link: '/views/projects/002',
+  },
+  {
+    name: 'vue3-admin-template',
+    link: '/views/projects/003',
+  },
+  {
+    name: 'vue3-admin-template',
+    link: '/views/projects/004',
+  },
+  {
+    name: 'vue3-admin-template',
+    link: '/views/projects/005',
+  },
+
+])
 </script>
 
 <template>
-  <div id="home">
-    <header class="text-center text-3xl mt20">
-      Vue3+ts template
-    </header>
-    <main class="mt20 flex justify-center items-center flex-col">
-      <img class="logo" src="/vite.svg" alt="logo">
-      <p class="text-lg">
-        The project is designed to fast develop a website.
-      </p>
-    </main>
-    <footer class="text-center mt10">
-      author: <a class="text-gray-700 no-underline hover:underline" href="https://github.com/windyeasy" target="_blank">windyeasy</a>
+  <div id="home" class="mx-auto max-w-[800px] m-t-[40px]">
+    <div class="title color-[#333] text-xl font-bold">
+      Projects
+    </div>
+    <div class="projects  grid gap-y-1 gap-x-4 sm:grid-cols-2 lg:grid-cols-3 p-t-5">
+      <template v-for="(item) in projects" :key="item.link">
+        <router-link :to="item.link" class="mr-4 link">
+          {{ item.name }}
+        </router-link>
+      </template>
+    </div>
+    <footer class="footer m-t-5 color-[#333]">
+      from 2024/10/27
     </footer>
   </div>
 </template>
