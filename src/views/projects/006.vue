a861f4cbeee11ca86a274dcc03b25fe24b350f66
windyeasy <windyeasy13gb@163.com>
2024-11-07 21:08:43 +0800

    feat: add 006

---
+++ src/views/projects/006.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const text = ref('')
+const content = 'Hello, World!'
+const DELAY = 200
+
+let timer: null | NodeJS.Timeout = null
+let i = 0
+function textAnimation() {
+  timer = setTimeout(() => {
+    if (i >= content.length) {
+      if (timer)
+        clearTimeout(timer)
+      return
+    }
+    text.value += content[i]
+
+    i++
+    textAnimation()
+  }, DELAY)
+}
+onMounted(() => {
+  textAnimation()
+})
+</script>
+
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
